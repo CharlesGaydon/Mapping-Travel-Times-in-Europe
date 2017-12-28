ce950c866190e3984f1c5468d27ce410c8439416
CharlesGaydon <charles@gaydon.fr>
2017-12-28 19:53:25 +0100

    Distance matrix taken into account.

--- docs/js/Dataviz_script.js
+++ docs/js/Dataviz_script.js
@@ -1,6 +1,6 @@
 console.log("START");
 var My_reference = undefined
-var alpha = 75;
+var alpha = 0.0089;
 
 function init() {
     mapFranceDisplay();
@@ -49,37 +49,62 @@ function mapFranceDisplay(){
             d.lat = parseFloat(d.lat)
             var projected_city = projection([d.long, d.lat])
             d.plong = projected_city[0]
-            d.plat = projected_city[1]                
+            d.plat = projected_city[1] 
+            d.dist = -1               
         }) 
         // associer à chaque ville un dictionniare avec pour chaque autre ville ses voisins.
+
+        d3.csv("data/French-cities_Distance_Matrix.txt",function(distances){
+            for(var city_index = 0 ; city_index<distances.length;city_index++){
+                var the_city = cities[city_index]["City"]
+                for(var dist_index = 0 ; dist_index<distances.length;dist_index++){
+                    if(distances[dist_index][""] == the_city){
+                        delete distances[dist_index][""]
+                        for(var key in distances[dist_index]){
+                           distances[dist_index][key] = parseFloat(distances[dist_index][key])
+                           if(isNaN(distances[dist_index][key])){
+                            distances[dist_index][key] = 15
+                           }
+                        }
+                        cities[city_index]["dist"] = distances[dist_index]
+                        break
+                    }
+                }
+            }
+        })
+        console.log(cities)
+        console.log(Object.keys(cities[0]))
+        cities.forEach(function(d){
+            console.log(d["dist"])  
+        })
         // Tous les import de données initiaux se font ici.
         var g = d3.select("g")
 
-    // STATIC CITIES
-    g.selectAll('.Static_Cities')
-            .data(cities)
-            .enter()
-            .append('circle')
-                .attr("class","Static_Cities")
-                .attr("cx", function(d) {return d.plong;})
-                .attr("cy", function(d) { return d.plat;})
-                //.attr("r", 3.5) //pas de rayon initialement !
-                .style("fill", "green")
-                .style("opacity",0.8)
-                .on("click",function(d){
-                    if(typeof My_reference !== 'undefined'){
-                        if(My_reference.City !=d.City){
-                            My_reference = {City :d.City, plong : d.plong, plat : d.plat};
-                            console.log(My_reference)
+     // STATIC CITIES
+        g.selectAll('.Static_Cities')
+                .data(cities)
+                .enter()
+                .append('circle')
+                    .attr("class","Static_Cities")
+                    .attr("cx", function(d) {return d.plong;})
+                    .attr("cy", function(d) { return d.plat;})
+                    //.attr("r", 3.5) //pas de rayon initialement !
+                    .style("fill", "green")
+                    .style("opacity",0.8)
+                    .on("click",function(d){
+                        if(typeof My_reference !== 'undefined'){
+                            if(My_reference.City !=d.City){
+                                My_reference = {City :d.City, plong : d.plong, plat : d.plat};
+                                console.log(My_reference)
+                            }else{
+                                My_reference = undefined;
+                            }
                         }else{
-                            My_reference = undefined;
+                            My_reference = {City :d.City, plong : d.plong, plat : d.plat};
                         }
-                    }else{
-                        My_reference = {City :d.City, plong : d.plong, plat : d.plat};
-                    }
-                        console.log("Ref :"+ My_reference)
-                    UpdateCitiesFrance();
-                });
+                            console.log("Ref :"+ My_reference.City)
+                        UpdateCitiesFrance();
+                    });
         // DYNAMIC CITIES
         g.selectAll('.Cities')
             .data(cities)
@@ -114,7 +139,7 @@ function mapFranceDisplay(){
                     }else{
                         My_reference = {City :d.City, plong : d.plong, plat : d.plat};
                     }
-                        console.log("Ref :"+ My_reference)
+                        console.log("Ref :"+ My_reference.City)
                     UpdateCitiesFrance();
                 });
     
@@ -159,8 +184,8 @@ function UpdateCitiesFrance(){
         // DYNAMIC CITIES
         g.selectAll(".Cities").filter(function(d){return My_reference.City != d.City })
             .transition().duration(300)
-            .attr("cx", function(d) {console.log(d);return new_coord(alpha, [My_reference.plong,My_reference.plat], [d.plong,d.plat])[0];} )
-            .attr("cy", function(d) {return new_coord(alpha, [My_reference.plong,My_reference.plat], [d.plong,d.plat])[1];} )
+            .attr("cx", function(d) {console.log(d.dist[My_reference.City]);return new_coord(alpha*d["dist"][My_reference.City], [My_reference.plong,My_reference.plat], [d.plong,d.plat])[0];} )
+            .attr("cy", function(d) {return new_coord(alpha*d["dist"][My_reference.City], [My_reference.plong,My_reference.plat], [d.plong,d.plat])[1];} )
             .attr("r", 4.5)
             .style("fill", "#7171D7");
 
