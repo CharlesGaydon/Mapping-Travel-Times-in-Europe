f127cbc0b5743a9b600d58cc05a14e5211a68b6b
Paul Peseux <32572118+ppmdatix@users.noreply.github.com>
2017-12-30 18:56:25 +0100

    oups

--- docs/js/Dataviz_script.js
+++ docs/js/Dataviz_script.js
@@ -174,21 +174,21 @@ function UpdateCitiesFrance(){
             .attr("opacity",0)
         // REBOUGER DANS L'AUTRE SENS LES VILLES ICI !
         g.selectAll(".Cities")
-            .transition().duration(transition_time)
+            .transition().duration(300)
             .attr("cx", function(d) {return d.plong;} )
             .attr("cy", function(d) {return d.plat;} )
             .attr("r", radius_static)
             .style("fill", dynamic_color);
 
         g.selectAll(".Static_Cities")
-            .transition().duration(transition_time)
+            .transition().duration(300)
             .attr("r", 0)
     
     }else{
 
         // DYNAMIC CITIES
         g.selectAll(".Cities").filter(function(d){return My_reference.City != d.City })
-            .transition().duration(transition_time)
+            .transition().duration(300)
             .attr("cx", function(d) {return new_coord(alpha*d["dist"][My_reference.City], [My_reference.plong,My_reference.plat], [d.plong,d.plat])[0];} )
             .attr("cy", function(d) {return new_coord(alpha*d["dist"][My_reference.City], [My_reference.plong,My_reference.plat], [d.plong,d.plat])[1];} )
             .attr("r", radius_dynamic)
@@ -196,14 +196,14 @@ function UpdateCitiesFrance(){
 
         g.selectAll(".Cities")
             .filter(function(d){return My_reference.City == d.City;})
-            .transition().duration(transition_time)
+            .transition().duration(300)
             .attr("cx", function(d){console.log("here",d.City); return d.plong;})
             .attr("cy", function(d){return d.plat;})
             .attr("r",radius_main)
             .style("fill",main_color)
 
         g.selectAll(".Static_Cities")
-            .transition().duration(transition_time)
+            .transition().duration(300)
             .attr("r", radius_static)
             .style("fill", static_color);
 
