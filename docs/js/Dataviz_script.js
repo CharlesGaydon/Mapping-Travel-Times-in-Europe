8fc101c89f441c6eaf75f9712a5f60a36e607063
Paul Peseux <32572118+ppmdatix@users.noreply.github.com>
2018-01-06 10:19:44 +0100

    oups

--- docs/js/Dataviz_script.js
+++ docs/js/Dataviz_script.js
@@ -16,9 +16,9 @@ France_color = "#97F2F2"; //before : "#7BCDC2"
 
 
 function init() {
-	mapEuropeDisplay();
-    //mapFranceDisplay();
-    //UpdateCitiesFrance();
+	//mapEuropeDisplay();
+    mapFranceDisplay();
+    UpdateCitiesFrance();
 }
 
 
@@ -341,7 +341,7 @@ function mapEuropeDisplay(){
     var path = d3.geoPath() // d3.geo.path avec d3 version 3
                  .projection(projection);
     
-    d3.json("data/europe_essai.json", function(json) {
+    d3.json("data/europe.json", function(json) {
       console.log(json)
       g.selectAll("path")
         .data(json.features)
